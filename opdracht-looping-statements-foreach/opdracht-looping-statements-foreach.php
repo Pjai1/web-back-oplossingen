e6dcc158085b99cc1fe19ec2a6f0ae7d57868bbf
Pjai1 <ulti40@hotmail.com>
2015-10-14 01:18:18 +0200

    Deadline 2

---
+++ opdracht-looping-statements-foreach/opdracht-looping-statements-foreach.php
@@ -0,0 +1,98 @@
+<?php
+
+    $text = file_get_contents('text-file.txt', FILE_USE_INCLUDE_PATH);
+    $textChars = array();
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht foreach</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+        
+            <h1>Opdracht foreach: deel 1</h1>
+
+            <ul>
+                <li>Maak een HTML-document met een PHP code-block</li> 
+
+                <li>
+                    Lees de tekst (text-file.txt) in en stop de tekst in een variabele <code>$text</code>
+                    <span class="tip">Misschien helpt de functie <code>file_get_contents</code></span>
+                </li>
+
+                <li>Splits de tekst op per karakter en sla deze op in een array <code>$textChars</code> ( dus een array die bestaat uit waarden van maximum 1 karakter)</li>
+                <?php
+                    //var_dump($textChars);
+                    //$textChars = str_split($text);
+                    $textChars = preg_split("//u", $text, -1, PREG_SPLIT_NO_EMPTY);
+                    //var_dump($textChars);
+                ?>
+                <li>Zorg ervoor dat deze array gesorteerd wordt van Z naar A</li>
+                <?php
+
+                    arsort($textChars);
+                    //var_dump($textChars);
+
+                ?>
+                <li>Draai nu de volgorde van de array volledig om</li>
+                <?php
+
+                    asort($textChars);
+
+                ?>
+
+                <li>Zorg er nu voor dat je via een for-lus alle karakters van de tekst overloopt en bijhoudt hoeveel keer elk karakter voorkomt. Toon een lijst met:
+                    <ul>
+                        <li>Hoeveel verschillende karakters er in totaal voorkomen</li>
+                        <?php
+
+                                foreach (count_chars($textChars, 1) as $i => $val) 
+                                {
+                                   echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.\n";
+                                }
+
+                        ?>
+                        <li>Hoeveel elk karakter voorkomt.</li>
+
+                    </ul>
+                </li>
+            </ul>
+
+            <h1 class="extra">Opdracht foreach: deel 2</h1>
+
+            <ul>
+                <li>Maak een HTML-document met een PHP code-block</li> 
+
+                <li>Maak een variabele <code>$tekst</code> met waarde (volledige inhoud van text-file.txt)</li>
+
+                <li>Analyseer hoe vaak elke letter van het alfabet voorkomt in de tekst (enkel de letters uit het alfabet, geen onderscheid tussen hoofdletters en kleine letters)</li>
+
+                <li>Toon de resultaten op het scherm, bv.
+                    
+                    <div class="facade-minimal" data-url="http://www.app.local/index.php">
+                        
+                        <ul>
+                            <li>E x 265</li>
+                            <li>A x 245</li>
+                            <li>...</li>
+                        </ul>
+
+                    </div>
+                    
+                    <p class="tip">Doe wat opzoekingswerk vooraleer je aan deze opdracht begint.</p>
+                </li>
+            </ul> 
+
+        </section>
+      
+
+    </body>
+</html>
